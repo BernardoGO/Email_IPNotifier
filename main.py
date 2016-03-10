93e914730b55ee9faac561ab3a9662c247c8763e
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-03-10 01:41:14 -0600

    initial

---
+++ main.py
@@ -0,0 +1,36 @@
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_user, gmail_pwd)
+    server.sendmail(FROM, TO, message)
+    server.close()
+    print 'successfully sent the mail'
+
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+ip = get_ip_address('enxfc8fc4081bee')
+send_email("bernardo.godinho.oliveira@gmail.com", "xxxx", "bernardo.godinho.oliveira@gmail.com", "IP ACK", "test");
